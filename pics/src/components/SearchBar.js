0f34db666e9cd5e965fd854a5677f6314a1f1751
cmulliss <cmulliss@gmail.com>
2020-02-21 16:59:52 +0000

    pics app started

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  state = {
+    term: ''
+  }
+  // dont want form to automatically submit itself
+  // need to use => syntax, or will get 'undefined'
+  onFormSubmit = (event) => {
+    event.preventDefault()
+
+    console.log(this.state.term)
+  }
+
+  render() {
+    return (
+      <div className='ui segment'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
+          <div className='field'>
+            <label>Image Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
+
+// // callback, called with one arg, the event object
+//onIputChange(event) {
+//console.log('event.target.value:', event.target.value)
+//}
+// class based as needs state
+// passing onChange={this.onIputChange} callback, as a prop, but no (), as don't want to call whenever component rendered.
+
+// SearchBar needs to be a controlled component
+
+// with controlled components can do things like force uppercase.
+// onChange={(e) =>
+// this.setState({ term: e.target.value.toUpperCase() })
