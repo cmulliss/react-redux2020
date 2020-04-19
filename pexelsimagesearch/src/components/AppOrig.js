ebadd04869d03327a0ce7eb9c13a3cd569697a6f
cmulliss <cmulliss@gmail.com>
2020-04-19 17:31:25 +0100

    images showing

--- pexelsimagesearch/src/components/AppOrig.js
+++ pexelsimagesearch/src/components/AppOrig.js
@@ -18,10 +18,10 @@ class App extends Component {
     const response = await pexels.get('/v1/search/', {
       params: { query: term, per_page: 10 },
     })
-    // once we get this response, we set it on our component state, which will cause our componen/videos/searcht to rerender and then we can print out the number of images fetched.
-    console.log('response.data.photos', response.data.photos)
+    // once we get this response, we set it on our component state, which will cause our component to rerender and then we can print out the number of images fetched.
+    console.log('response.data.total_results', response.data.total_results)
     // update my state
-    this.setState({ images: response.data.photos })
+    this.setState({ images: response.data.total_results })
   }
   // Inside the SearchBar, to reference the prop we say props.onSubmit
   render() {
--- pexelsimagesearch/src/components/ImageList.js
+++ pexelsimagesearch/src/components/ImageList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const ImageList = () => {
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <img src={image.src.small} key={image.id} alt={image.photographer} />
+  })
+
+  console.log('props.images', props.images)
+
   return (
     <div>
-      <div>ImageList</div>
+      <div>{images}</div>
     </div>
   )
 }
-
 export default ImageList
+// using props to communicate the list of images down from App.
+// assign the [] that gets created, to images, then reference that variable inside the div.
