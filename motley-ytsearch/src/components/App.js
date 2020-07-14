14567f909871c4ecfa9ebe88d4d0d0a6785e74f3
cmulliss <cmulliss@gmail.com>
2020-07-14 18:18:13 +0100

    key working

--- motley-ytsearch/src/components/App.js
+++ motley-ytsearch/src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
+import youtube from '../apis/youtube'
 import SearchBar from './SearchBar'
 
+const KEY = 'AIzaSyCuQi9RmHLL97Hkbrp5-uzGBueZDP5fM7k'
+
 class App extends Component {
+  onTermSubmit = (term) => {
+    youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
+      },
+    })
+    console.log('term', term)
+  }
   render() {
     return (
       <div className='ui container'>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     )
   }
@@ -14,4 +29,43 @@ class App extends Component {
 
 export default App
 
-// AIzaSyCuQi9RmHLL97Hkbrp5-uzGBueZDP5fM7k
+/*
+We want to take the list of videos received and set them as state on our App component. That's going to allow App to update itself, or render itself, which is then going to allow us to render the newly fetched videos as a list to the screen.
+
+Add a callback method to our class App, that will be called anytime someone submits the SearchBar form, onTermSubmit, that will be called with some search string. Then add this as a prop to <SearchBar />. Then go to SearchBar.js.
+
+const KEY = 'AIzaSyCuQi9RmHLL97Hkbrp5-uzGBueZDP5fM7k'
+
+Axios Version Bug & 400 "Required Parameter: part" Error
+Updated 3-18-2020
+
+In the upcoming lecture, we will be creating an axios config instance to call the YouTube API. Starting with release v0.19.0 there is a bug that fails to merge the parameters from the config instance. This will cause a 400 error failure with the message "Required Parameter : part"
+
+Here are two ways we can deal with this:
+1. Install a specific version of axios:
+npm install axios@0.18.1
+2. Move the Params object to the App.js axios call
+
+This is what your axios code should look like at the end of "Putting it all Together" lecture
+
+apis/youtube.js:
+
+import axios from "axios";
+ 
+export default axios.create({
+     baseURL: "https://www.googleapis.com/youtube/v3"
+});
+App.js:
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: 'video',
+        key: KEY
+      }
+    });
+
+*/
--- motley-ytsearch/src/components/SearchBar.js
+++ motley-ytsearch/src/components/SearchBar.js
@@ -3,13 +3,28 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
   state = { term: '' }
 
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    // call with this.state.term, so parent is told what the current search term is.
+    this.props.onFormSubmit(this.state.term)
+  }
+  // above, todo: make sure we call callback from parent component, so we can tell the app the user just submitted the form and they probably want to init a new search of the youtube api.
+
   render() {
     return (
       <div className='search-bar ui segment'>
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Video Search</label>
-            <input type='text' value={this.state.term} />
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
@@ -18,3 +33,14 @@ class SearchBar extends Component {
 }
 
 export default SearchBar
+
+/*
+ You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.
+    in input (at SearchBar.js:12)
+
+    get the text working
+    or could use:
+    onChange={e => this.setState({term: e.target.value})}
+
+    then need a callback handler on our form too.
+*/
